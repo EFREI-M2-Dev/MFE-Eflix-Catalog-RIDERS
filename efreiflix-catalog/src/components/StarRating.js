e2360986cb5d24730d9c2b0519825d5eca14bf5b
Florent Paris <florentparis28@gmail.com>
2025-02-14 09:12:23 +0100

    fix(rating): rating display decimal

--- efreiflix-catalog/src/components/StarRating.js
+++ efreiflix-catalog/src/components/StarRating.js
@@ -1,28 +1,29 @@
-import React from "react";
-import StarIcon from "../assets/star";
+import React from 'react';
+import StarIcon from '../assets/star';
 
 const StarRating = ({ rating }) => {
-    const fullStars = Math.floor(rating);
-    const decimalPart = rating - fullStars;
-    const starElements = [];
+  const fullStars = Math.floor(rating);
+  const decimalPart = rating - fullStars;
+  const starElements = [];
 
-    for (let i = 0; i < fullStars; i++) {
-        starElements.push(<StarIcon />);
-    }
+  for (let i = 0; i < fullStars; i++) {
+    starElements.push(<StarIcon />);
+  }
 
-    if (decimalPart > 0) {
-        starElements.push(
-            <div className="w-7">
-                <div
-                    className={`overflow-hidden w-[calc(28px*${decimalPart.toFixed(1)})]`}
-                >
-                    <StarIcon />
-                </div>
-            </div>
-        );
-    }
+  if (decimalPart > 0) {
+    starElements.push(
+      <div className="w-7">
+        <div
+          style={{ width: `calc(28px*${decimalPart.toFixed(1)})` }}
+          className={`overflow-hidden`}
+        >
+          <StarIcon />
+        </div>
+      </div>
+    );
+  }
 
-    return <div className="flex">{starElements}</div>;
+  return <div className="flex">{starElements}</div>;
 };
 
 export default StarRating;
