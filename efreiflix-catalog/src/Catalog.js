74407520b32a967f29e2c09298c0e24c1b3652ca
Florent Paris <florentparis28@gmail.com>
2025-03-06 09:48:34 +0100

    fix(catalog): reduce movie by genre

--- efreiflix-catalog/src/Catalog.js
+++ efreiflix-catalog/src/Catalog.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './styles.css';
 import { fetchData } from '../service/catalog';
 import MovieCard from './components/MovieCard';
-import { Swiper } from 'swiper/react';
-import 'swiper/css';
+import './styles/index.css';
 
 const Catalog = () => {
   const [data, setData] = useState(null);
@@ -12,6 +11,7 @@ const Catalog = () => {
     const getData = async () => {
       try {
         const result = await fetchData();
+        console.log(result);
         setData(result);
       } catch (e) {
         console.error('Failed to fetch data:', e);
@@ -23,18 +23,34 @@ const Catalog = () => {
 
   if (!data) return <div>Loading...</div>;
 
+  const moviesByGenre = data.reduce((acc, movie) => {
+    movie.genres.forEach((genre) => {
+      if (!acc[genre]) acc[genre] = [];
+      acc[genre].push(movie);
+    });
+    return acc;
+  }, {});
+
   return (
-    <Swiper
-      spaceBetween={20}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      navigation
-    >
-      {data.map((movie) => {
-        return <MovieCard key={movie.id} movie={movie} />;
-      })}
-    </Swiper>
+    <div className="flex flex-col gap-8">
+      {Object.entries(moviesByGenre).map(([genre, movies], index) => (
+        <div
+          key={genre}
+          style={{
+            zIndex: Object.entries(moviesByGenre).length - index,
+          }}
+        >
+          <h2 className="text-xl font-bold mb-4">{genre}</h2>
+          <div className="hide-scrollbar pl-2 -ml-2 pt-2 -mt-2 pb-28 -mb-28 overflow-x-auto whitespace-nowrap">
+            <div className="flex gap-4">
+              {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
--- efreiflix-catalog/src/components/MovieCard.js
+++ efreiflix-catalog/src/components/MovieCard.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import StarRating from './StarRating';
 import Tag from './Tag';
-import { SwiperSlide } from 'swiper/react';
 
 const MovieCard = ({ movie }) => {
   return (
-    <SwiperSlide className="group hover:scale-[1.02] duration-200 rounded-md group flex flex-col gap-1 justify-center cursor-pointer">
+    <div className="w-full min-w-[270px] max-w-[270px] group hover:scale-[1.02] duration-200 rounded-md group flex flex-col gap-1 justify-center cursor-pointer">
       <div className="relative flex items-center justify-center w-full aspect-video">
         <div className="rounded-t-md hidden group-hover:flex flex-col absolute top-0 items-center justify-center left-0 bg-gray-900 text-white h-full w-full bg-opacity-50">
           <div>
@@ -28,7 +27,7 @@ const MovieCard = ({ movie }) => {
           style={{ backgroundImage: `url(${movie.posterUrl})` }}
         ></div>
       </div>
-    </SwiperSlide>
+    </div>
   );
 };
 
